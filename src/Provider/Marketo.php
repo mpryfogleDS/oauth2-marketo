2dc4f97a3419bbc9f2c81ff024518c50129f0074
Michael Pryfogle <mpryfogle@designsensory.com>
2021-05-03 10:33:26 -0400

    ready to go?

--- src/Provider/Marketo.php
+++ src/Provider/Marketo.php
@@ -2,59 +2,86 @@
 
 namespace Kristenlk\OAuth2\Client\Provider;
 
-use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Grant\AbstractGrant;
+use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
+use Kristenlk\OAuth2\Client\Token\AccessToken as MarketoAccessToken;
 use Psr\Http\Message\ResponseInterface;
 
 class Marketo extends AbstractProvider
 {
     protected $baseUrl;
 
+    /**
+     * The base marketo API url.
+     *
+     * @return string
+     */
     public function getBaseUrl()
     {
         return $this->baseUrl;
     }
 
     /**
-     * Returns the base URL for requesting an access token.
-     *
-     * @param array $params
-     * @return string
+     * {@inheritDoc}
      */
     public function getBaseAccessTokenUrl(array $params)
     {
-        return $this->getBaseUrl() . "/identity/oauth/token";
-    }
-
-    protected function createAccessToken(array $response, AbstractGrant $grant)
-    {
-        return new \Kristenlk\OAuth2\Client\Token\AccessToken($response);
+        return $this->getBaseUrl() . '/identity/oauth/token?' . http_build_query($params);
     }
 
     /**
-     * Check a provider response for errors.
-     *
-     * @param ResponseInterface $response
-     * @param array|string $data
-     *
-     * @throws IdentityProviderException
+     * {@inheritDoc}
+     */
+    protected function createAccessToken(array $response, AbstractGrant $grant)
+    {
+        return new MarketoAccessToken($response);
+    }
+
+    /**
+     * {@inheritDoc}
      */
     protected function checkResponse(ResponseInterface $response, $data)
     {
         if ($response->getStatusCode() >= 400) {
             throw new IdentityProviderException(
-                $data['error'] ?: $response->getReasonPhrase(),
+                isset($data['error']) ? $data['error'] : $response->getReasonPhrase(),
                 $response->getStatusCode(),
                 $response
             );
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return '';
+    }
 
-    public function getBaseAuthorizationUrl() {}
-    public function getResourceOwnerDetailsUrl(AccessToken $token) {}
-    protected function getDefaultScopes() {}
-    protected function createResourceOwner(array $response, AccessToken $token) {}
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        throw new \Exception('Not Implemented', 501);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getDefaultScopes()
+    {
+        return NULL;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        throw new \Exception('Not Implemented', 501);
+    }
 }
